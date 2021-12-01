5ee24ad1dbcbee5546ba4dfd7c275cb0d613bcd1
rvsia <r.vsia@seznam.cz>
2021-12-01 17:47:06 +0100

    Do not purge dynamic classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,20 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    safelist: [
+      //['purple', 'green', 'red', 'yellow', 'blue']
+      'text-purple-400',
+      'border-purple-400',
+      'text-green-400',
+      'border-green-400',
+      'text-red-400',
+      'border-red-400',
+      'text-yellow-400',
+      'border-yellow-400',
+      'text-blue-400',
+      'border-blue-400',
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
