d2827f468312c89b94cc1a316db0303607f052ad
rvsia <r.vsia@seznam.cz>
2021-12-02 17:43:42 +0100

    Add booze

---
+++ src/projects/CheapBooze.js
@@ -0,0 +1,10 @@
+export default {
+    name: 'Cheap Booze as a Service',
+    hackathon: 'August 2021',
+    authors: [{name: 'Stephen Adams'}, { name: 'Ryan Long'}, {name: 'Alec Cohan'}, {name: 'Chris Mitchel'}],
+    link: 'https://www.2dorks.net/media/hnh/shows/specials/cbaas-demo.m4v',
+    additionalLinks: [],
+    labels: ['Fun'],
+    description: 'Leverage the NC ABC Liquor quarterly price list to build some kind of nifty thing. Maybe pair it with a cocktail DB to help price a cocktail. Maybe do recommendations for cheap but tasty beverages.',
+    image: '/Booze.png'
+}
--- src/projects/index.js
+++ src/projects/index.js
@@ -1,5 +1,6 @@
 import ArduinoHR from './ArduinoHR';
+import CheapBooze from './CheapBooze';
 import InsightsGivesBack from './InsightsGivesBack';
 import MarioGame from './MarioGame';
 import StorWars from './StorWars';
-export default [ArduinoHR,InsightsGivesBack,MarioGame,StorWars]
+export default [ArduinoHR,CheapBooze,InsightsGivesBack,MarioGame,StorWars]
