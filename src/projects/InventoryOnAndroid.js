eee94ff847bb7fe7487f01b4795bf925826f24fe
Karel Hala <khala@redhat.com>
2021-12-03 16:59:19 +0100

    Add Inventory on Android

---
+++ src/projects/InventoryOnAndroid.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'CRC inventory for android',
+  hackathon: 'May 2021',
+  authors: [{name: 'Karel Hala'}, {name: 'Ingrid Sena'}, {name: 'Priscila Gutierres'}],
+  link: 'https://github.com/karelhala/hackathon-2021',
+  additionalLinks: [
+    {label: 'Trailer', link: 'https://drive.google.com/file/d/1TrvXkz2sduekpDc2FS0ym_I5e8PuTs7A/view'}
+  ],
+  labels: ['ConsoleDot'],
+  description: 'Android app that logs into ConsoleDot and shows Inventory data.',
+  image: '/InventoryOnAndroid.png',
+  keywords: ['Android', 'Inventory']
+}
