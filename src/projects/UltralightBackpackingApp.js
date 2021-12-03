34426efb3f1bf95b39e6e810a1e38beb31f969f8
Richard Všianský <32869456+rvsia@users.noreply.github.com>
2021-12-03 19:56:24 +0100

    Update UltralightBackpackingApp.js

--- src/projects/UltralightBackpackingApp.js
+++ src/projects/UltralightBackpackingApp.js
@@ -4,7 +4,7 @@ export default {
     authors: [{name: 'Filip Hlaváč'}, { name: 'Radek Kalužík'}, {name: 'Eric Winchell'}],
     link: 'https://drive.google.com/file/d/1xO4WqtrtcWt0Psg7XS5MGpCkguo7CGqi/view?usp=sharing',
     additionalLinks: ['https://github.com/fhlavac/backpacking-frontend', 'https://github.com/fhlavac/backpacking'],
-    labels: [],
+    labels: ['Fun'],
     description: 'Packing a backpack for a hiking trip is often stressful. Especially when you want to carry the lowest possible weight on your backs. The application should serve as a catalog of outdoor equipment, which will allow you to combine the necessary equipment appropriately to both avoid carrying extra weight and forgetting essentials at home.',
     image: '/backpacking.png',
     keywords: ['React', 'Node.js', 'Hiking', 'Tools']
