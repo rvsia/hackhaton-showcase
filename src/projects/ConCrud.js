1785c0488c30d7eb9f044d0cd3d322e360590ea5
Stephen Adams <stephen.adams@redhat.com>
2022-06-01 16:11:02 -0400

    add con crud to project list
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

---
+++ src/projects/ConCrud.js
@@ -0,0 +1,17 @@
+// ConCrud.js
+export default {
+    name: 'Con Crud: A Text Adventure',
+    hackathon: 'May 2022',
+    authors: [{
+        name: 'Stephen Adams'
+    }, {
+        name: 'Josejulio Martinez'
+    }, {
+        name: 'Alec Cohan'
+    }],
+    link: 'https://github.com/adventureboss/hackathon-adventure',
+    labels: ['Games', 'Fun'],
+    description: 'A text adventure game where you are an attendee at a con and have to save the day',
+    image: '/concrud.png',
+    keywords: ['Godot']
+}
