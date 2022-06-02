1924b3a7df88ad7ece2d582f917e678da9bf798b
Stephen Adams <stephen.adams@redhat.com>
2022-06-02 12:04:52 -0400

    Add AdminQuest project
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

---
+++ src/projects/AdminQuest.js
@@ -0,0 +1,23 @@
+// ConCrud.js
+export default {
+    name: 'AdminQuest: A Sysadmin Learning Adventure',
+    hackathon: 'May 2022',
+    authors: [{
+        name: 'Stephen Adams'
+    }, {
+        name: 'Josejulio Martinez'
+    }, {
+        name: 'Alec Cohan'
+    }, {
+        name: 'Andressa Cabistani'
+    }, {
+        name: 'Isha Kanodia'
+    }, {
+        name: 'Justin Orringer'
+    }],
+    link: 'https://github.com/adventureboss/adminquest',
+    labels: ['Games', 'Fun'],
+    description: 'A sysadmin themed RPG where you are a sysadmin and have to save the day',
+    image: '/adminquest.png',
+    keywords: ['Godot', 'RPG']
+}
