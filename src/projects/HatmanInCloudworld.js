830bd6467eb8385e230fbbc141d6c5a5865de779
Adam Drew <addrew@redhat.com>
2022-12-12 10:48:31 -0500

    Added Hatman in Cloudworld from Dec 22 Hackathon

---
+++ src/projects/HatmanInCloudworld.js
@@ -0,0 +1,12 @@
+// ConCrud.js
+export default {
+    name: 'Hatman in Cloudworld',
+    hackathon: 'May 2022',
+    authors: [{name: 'Adam Drew'}],
+    link: 'https://github.com/adamrdrew/hatman-in-cloudworld',
+    additionalLinks: [],
+    labels: ['Games', 'Fun'],
+    description: 'A simple platformer game for the NES written in 6502 assembly',
+    image: '/hatman.png',
+    keywords: ['NES', '6502', 'Assembly']
+}
