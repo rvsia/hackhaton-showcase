501cd51f5739df5965b00066cb1f961427d16895
Richard Všianský <richard.vsiansky@sap.com>
2023-03-08 08:43:10 +0100

    Fix go snake

--- src/projects/GoSnake.js
+++ src/projects/GoSnake.js
@@ -1,14 +1,16 @@
 export default {
-    "hackathon": "March 2023",
-    "labels": ["Games", "Fun"],
-    "name": "GoSnake",
-    "description": "The classic game written in GoLang.",
-    "authors": [
-        {
-            "name": "Brandon Schneider"
-        }, {
-	    "name": "Richard Brantley"
-	}],
-    "image": "/GoSnake.png",
-    "keywords": ["Go", "2D"]
+  hackathon: "March 2023",
+  labels: ["Games", "Fun"],
+  name: "GoSnake",
+  description: "The classic game written in GoLang.",
+  authors: [
+    {
+      name: "Brandon Schneider",
+    },
+    {
+      name: "Richard Brantley",
+    },
+  ],
+  image: "/GoSnake.png",
+  keywords: ["Go", "2D"],
 };
